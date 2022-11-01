883a640b0e148d3eb2cbfc449b019e84ed37faaa
rg98098 <rg98098@gmail.com>
2022-11-01 11:28:25 +0530

    seach feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello App</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+    }
+    console.log("constructor")
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount")
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users=> this.setState(()=> {
+      return {monsters:users}
+    }))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(()=> {
+
+      return {
+        searchField
+      }
+    })
+  }
+
+  render(){
+    console.log("render")
+
+    const { monsters, searchField} = this.state
+    const {onSearchChange} = this
+    const filterMonster = monsters.filter(monster=> {
+      return monster.name.toLowerCase().includes(searchField)
+    })
+    return <div className="App">
+    <input className="search-box" type="search" placeholder="search monsters" onChange={onSearchChange}/>
+    {filterMonster?.map((monster)=>{
+      return <div key={monster.id}>
+        <h1>{monster.name}</h1>
+      </div>
+    })}
+    </div>;
+  }
 }
 
 export default App;
